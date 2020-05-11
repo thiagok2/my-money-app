ad7c0cfea9eef5d1cf8b2987e9105116b6ecbcb0
Thiago Oliveira <thiagok2@gmail.com>
2020-05-10 23:15:35 -0300

    135. Componente Menu (Parte 2)

--- frontend/src/common/template/menu.jsx
+++ frontend/src/common/template/menu.jsx
@@ -7,7 +7,7 @@ export default props => (
     <ul className='sidebar-menu'>
         <MenuItem path='#' label='Dashboard' icon='dashboard' />
         <MenuTree label='Cadastro' icon='edit'> 
-            <MenuItem path='#billingCycles'
+            <MenuItem path='#billingCycles1'
                 label='Ciclos de Pagamentos' icon='usd' />
         </MenuTree>
     </ul>
