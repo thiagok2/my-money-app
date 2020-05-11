8efca0fbe47953073983cfd319897064b9d7ce9c
Thiago Oliveira <thiagok2@gmail.com>
2020-05-10 23:17:23 -0300

    136. Footer

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -4,11 +4,13 @@ import '../common/template/dependencies'
 
 import Header from '../common/template/header'
 import SideBar from '../common/template/sideBar'
+import Footer from '../common/template/footer'
 
 export default props => (
     <div className='wrapper'>
         <Header></Header>
         <SideBar></SideBar>
+        <Footer></Footer>
 
     </div>
 )
