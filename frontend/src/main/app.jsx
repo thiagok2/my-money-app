9ac1b32c9b573af7923ec2a165a791b363ed3a60
Thiago Oliveira <thiagok2@gmail.com>
2020-05-10 23:06:49 -0300

    134. Componente Menu (Parte 1)

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -3,10 +3,12 @@ import React from 'react'
 import '../common/template/dependencies'
 
 import Header from '../common/template/header'
+import SideBar from '../common/template/sideBar'
 
 export default props => (
     <div className='wrapper'>
         <Header></Header>
-        
+        <SideBar></SideBar>
+
     </div>
 )
