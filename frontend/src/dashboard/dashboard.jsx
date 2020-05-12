de1689f7fdb4c9c33a58e925ee99980d77da4322
Thiago Oliveira <thiagok2@gmail.com>
2020-05-11 22:46:13 -0300

    142. Usando ValueBox

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -3,14 +3,27 @@ import React, { Component } from 'react'
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
 
+import ValueBox from  '../common/widget/valueBox'
+import Row from  '../common/layout/row'
+
 class Dashboard extends Component {
     
     render(){
+
+        const credit = 1000
+        const debt = 500
         return (
             <div> 
                 <ContentHeader title='Dashboard' small='Versão 1.0' />
                 <Content>
-                    Dashboard
+                    <Row> 
+                        <ValueBox cols='12 4' color='green' icon='bank'
+                            value={`R$ ${credit}`} text='Total de Créditos' />
+                        <ValueBox cols='12 4' color='red' icon='credit-card'
+                            value={`R$ ${debt}`} text='Total de Débitos' />
+                        <ValueBox cols='12 4' color='blue' icon='money'
+                            value={`R$ ${credit - debt}`} text='Valor Consolidado' />
+                    </Row> 
                 </Content>
             </div>
         )
